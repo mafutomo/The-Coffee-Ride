8be8e3e1d3fd410f3e0d0c38837d455759fdbfa8
Melissa Utomo <mafutomo@gmail.com>
2018-04-09 13:03:19 -0600

    updated with ES6

--- script.js
+++ script.js
@@ -41,15 +41,18 @@ $(document).ready(function() {
     updateModal()
   })
 
-
   $('.card-content a').click(function(event) {
     event.preventDefault();
+    //Gathering selected content Coffee Name and the Price
     var $target = $(event.target);
     var $itemName = $target.siblings()[0].innerText;
     itemName = $itemName.slice(0, -12);
     var $itemPrice = $target.siblings()[2].innerText;
     itemPrice = Number($itemPrice.replace(/[^0-9\.]+/g, ""));
+
     updateTotalVal(itemPrice, tax)
+
+    //If the item already exists, then add the quantity. If not create a new key value pair
     if (cart.hasOwnProperty(itemName) === false) {
       cart[itemName] = [itemPrice, 1];
       localStorage.setItem('cart', JSON.stringify(cart));
@@ -61,8 +64,11 @@ $(document).ready(function() {
   });
 
   $('.card-reveal a').click(function(event) {
+
     event.preventDefault();
+
     var $target = $(event.target);
+    console.log("TARGET from card-reveal", $target);
     var $itemName = $target.parentsUntil('.card').siblings()[2].innerText;
     itemName = $itemName.slice(0, -5);
     var $itemPrice = $target.parents('.card').children().children()[3].innerText;
@@ -77,6 +83,6 @@ $(document).ready(function() {
     }
     updateModal();
   });
-  
+
   $('.modal').modal();
 });
