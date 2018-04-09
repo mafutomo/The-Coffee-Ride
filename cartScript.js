c3fc4987565bfba9455d7a29b147e9f10ae2974a
Melissa Utomo <mafutomo@gmail.com>
2018-04-09 12:23:50 -0600

    updated logic to template literal, added .toFixed(2) for money amount

--- cartScript.js
+++ cartScript.js
@@ -6,43 +6,45 @@ $('document').ready(function() {
   var taxedAmount = (subtotal * tax) - subtotal;
   var total = subtotal + taxedAmount;
 
+  //if cart is empty
   if(jQuery.isEmptyObject(cart)){
     $('#cart-final').append(
-      "<tr>" +
-      "<td>" +
-      "Nothing Selected Yet..." +
-      "</td>" +
-      "<td>" +
-      "</td>" +
-      "<td>" +
-      "</td>" +
-      "</tr>");
+      `<tr>
+      <td>
+      Nothing Selected Yet...
+      </td>
+      <td>
+      </td>
+      <td>
+      </tr>`
+    );
   }
   //To update table
   for (let key in cart) {
     $('#cart-final').append(
-      "<tr>" +
-      "<td>" +
-      key +
-      "</td>" +
-      "<td>" +
-      cart[key][1] +
-      "</td>" +
-      "<td>" +
-      "$  " +
-      cart[key][0] +
-      "</td>" +
-      "</tr>");
+      `<tr>
+      <td>
+      ${key}
+      </td>
+      <td>
+      ${cart[key][1]}
+      </td>
+      <td>
+      $
+      ${cart[key][0].toFixed(2)}
+      </td>
+      </tr>`
+    );
   }
 
   //To update subtotal
-  $("#subtotal-val").empty().text("$  " + subtotal);
+  $("#subtotal-val").empty().text(`$ ${subtotal.toFixed(2)}`);
 
   //To update tax amount
-  $("#tax-val").empty().text("$  " + taxedAmount.toFixed(2));
+  $("#tax-val").empty().text(`$  ${taxedAmount.toFixed(2)}`);
 
   //To update total amount
-  $("#total-val").empty().text("$  " + total.toFixed(2));
+  $("#total-val").empty().text(`$  ${total.toFixed(2)}`);
 
   //Clear Cart
   $("#clear-cart").click(function(event) {
